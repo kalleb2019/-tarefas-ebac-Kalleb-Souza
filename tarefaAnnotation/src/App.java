3a951d81d16a492376ecddc5284715b45a8017b1
Kalleb <souzakalleb73@gmail.com>
2023-08-13 20:44:55 -0300

    Commit anotação

---
+++ tarefaAnnotation/src/App.java
@@ -0,0 +1,7 @@
+
+ @Tabela(value = "tabela1")
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    }
+}
---
+++ tarefaAnnotation/src/Tabela.java
@@ -0,0 +1,9 @@
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.TYPE)
+public @interface Tabela {
+    String value();
+}
