786eb81cc367276f4ad74a32a099eb1c092dddd8
Kalleb <Kallebpradosi@gmail.com>
2024-05-10 21:45:05 -0300

    ex

--- Projeto3/src/br/com/souza/dao/IClienteDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.souza.dao;
-
-import br.com.souza.dao.generics.IGenericDao;
-import br.com.souza.domain.Cliente;
-
-public interface IClienteDao extends IGenericDao<Cliente, Long> {
-}
--- Projeto3/src/br/com/souza/dao/IProdutoDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.souza.dao;
-
-import br.com.souza.dao.generics.IGenericDao;
-import br.com.souza.domain.Produto;
-
-public interface IProdutoDao extends IGenericDao<Produto, Long> {
-}
